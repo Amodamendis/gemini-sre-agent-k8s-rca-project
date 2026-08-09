0ea43da313d49c64ef83a5f8812f5d281857b9b0
Amodamendis <amodarashmika2003@gmail.com>
2026-08-09 08:33:31 +0530

    updated with model 3.5

--- src/llm_engine.py
+++ src/llm_engine.py
@@ -13,7 +13,7 @@ class SREAgentBrain:
             raise ValueError("GEMINI_API_KEY environment variable is missing")
         
         self.client = genai.Client()
-        self.model = "gemini-1.5-flash"
+        self.model = "gemini-3.5-flash"
 
         self.system_instruction = """
 You are a Senior Kubernetes Site Reliability Engineer (SRE). 
